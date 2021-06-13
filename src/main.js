e639e0ee2ad3c10da388a56ffe58cc92a0a0e927
Nerlih Max <nerlihmax@yandex.ru>
2021-06-13 13:48:15 +0700

    merg mainjs

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 
 import './assets/styles/index.pcss';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-Axios.defaults.baseURL = 'https://tenderino.herokuapp.com';
+// Axios.defaults.baseURL = 'https://tenderino.herokuapp.com';
+Axios.defaults.baseURL = 'http://10.129.0.252:3030';
 
 createApp(App).use(router).mount('#app');
